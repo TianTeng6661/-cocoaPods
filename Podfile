80bfe5c22bd2c865760b769c0974efb097b7b0c6
Mr.Tian <2127392291@qq.com>
2016-08-12 15:32:10 +0800

    cocoaPods更新

--- Podfile
+++ Podfile
@@ -1,4 +1,9 @@
 platform :ios, '8.0'
+
+target ‘软件框架’do
+
 pod 'AFNetworking','~> 3.0.4'
 pod 'SDWebImage','~>3.7.5'
 pod 'MBProgressHUD', '~>0.9.2'
+
+end
--- 软件框架/FirstViewController.m
+++ 软件框架/FirstViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     self.view.backgroundColor=[UIColor redColor];
     // Do any additional setup after loading the view.
-    
+
     self.view.backgroundColor = [UIColor whiteColor];
 }
 
